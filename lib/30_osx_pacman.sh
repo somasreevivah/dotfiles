ba50b4782970fdbf9bd65e802f1e69a3590e2862
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-02-15 15:20:15 +0100

    Added pacman for osx and ubuntu

---
+++ lib/30_osx_pacman.sh
@@ -0,0 +1,7 @@
+
+# Package management
+alias dfs::update="brew -qq update && sudo brew upgrade"
+alias dfs::install="brew install"
+alias dfs::uninstall="brew uninstall"
+alias dfs::remove="brew remove"
+alias dfs::search="brew search"
