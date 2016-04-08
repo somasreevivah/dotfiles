d531cfeda9e471157bb986f768892319a6b1db4a
alejandrogallo <gallo@abakus01.fkf.mpg.de>
2016-04-08 11:38:26 +0200

    alias

--- lib/50_fkf.sh
+++ lib/50_fkf.sh
@@ -33,3 +33,5 @@ PERL_LOCAL_LIB_ROOT="/home/gallo/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_R
 PERL_MB_OPT="--install_base \"/home/gallo/perl5\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=/home/gallo/perl5"; export PERL_MM_OPT;
 
+# ALIASES
+alias aba="ssh abakus01"
