22d10178c6e24a5ac049910d96570abd599de427
nango <nango@locahost.localdomain>
2016-04-15 17:57:15 +0000

    Added term stuff

---
+++ lib/50_term.sh
@@ -0,0 +1,19 @@
+#/**************************************************************************/
+#/* -bd color                    border color                              */
+#/* -/+bdc                       turn off/on display of bold as color      */
+#/* -bg color                    background color                          */
+#/* -/+cm                        turn off/on ANSI color mode               */
+#/* -cr color                    text cursor color                         */
+#/* -/+dc                        turn off/on dynamic color selection       */
+#/* -fg color                    foreground color                          */
+#/* -/+hm                        turn on/off selection-color override      */
+#/* -ms color                    pointer color                             */
+#/* -/+pc                        turn on/off PC-style bold colors          */
+#/* -report-colors               report colors as they are allocated       */
+#/* -/+rvc                       turn off/on display of reverse as color   */
+#/* -selbg color                 selection background color                */
+#/* -selfg color                 selection foreground color                */
+#/* -/+ulc                       turn off/on display of underline as color */
+#/**************************************************************************/
+
+alias xterm="xterm -maximized -bg black -cr red -fg green"
