a846a75e99f21ecd6f98514baeddd9bb74c057e2
nango <nango@locahost.localdomain>
2016-04-15 21:54:24 +0000

    lynx cfg

--- lib/50_browser.sh
+++ lib/50_browser.sh
@@ -1,6 +1,7 @@
 
 if [[ $TERM = "linux" || $TERM = "screen" ]]; then
   which lynx &> /dev/null && export BROWSER="lynx"
+  export LYNX_CFG=$HOME/.lynx.cfg
 else
   if dfs::is_osx ; then
     export BROWSER="open -a Safari"
