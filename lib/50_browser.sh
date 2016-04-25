4bc155c9154d39372975b4f57f290317e075bac9
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-04-25 13:58:24 +0200

    browser fix

--- lib/50_browser.sh
+++ lib/50_browser.sh
@@ -1,7 +1,11 @@
 
-if [[ $TERM = "linux" || $TERM = "screen" ]]; then
-  which lynx &> /dev/null && export BROWSER="lynx"
-  export LYNX_CFG=$HOME/.lynx.cfg
+if [[ $TERM = "linux" || $TERM = "screen"  || $TERM =~ "xterm" ]]; then
+  if which elinks; then
+    export BROWSER="elinks" #I prefer elinks, it is nice 
+  elif which lynx; then
+    export BROWSER="lynx"
+    export LYNX_CFG=$HOME/.lynx.cfg
+  fi
 else
   if dfs::is_osx ; then
     export BROWSER="open -a Safari"
