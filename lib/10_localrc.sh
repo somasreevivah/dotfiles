bcc3998231abbc11816c640f47851ecb9f5f7246
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-05-10 08:56:37 +0000

    added localrc

---
+++ lib/10_localrc.sh
@@ -0,0 +1,12 @@
+
+  #This file is to source local configuration files,
+  #in our case it is just for the bash configuration file.
+
+LOCAL_CONFIGURATION_FILES=(
+$HOME/.bashrc_local
+)
+
+
+for file in ${LOCAL_CONFIGURATION_FILES[@]}; do
+  [ -f $file ] && source $file
+done
