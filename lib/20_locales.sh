48e0b8bdc1427c5f3cc6853daababeecc952625f
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-05-12 08:50:05 +0000

    stuff

--- lib/20_locales.sh
+++ lib/20_locales.sh
@@ -6,25 +6,28 @@
 
 
 
-#if dfs_is_cluster_abakus; then
-  ##I do not know if this is necessary
-  #export LC_CTYPE=C
-#else
-  #continue
-  export LANG=en_US.UTF-8
-  export LANGUAGE=en_US.UTF-8
-  export LC_MESSAGES="en_US.UTF-8"
-  export LC_CTYPE="en_US.UTF-8"
-  ##export LANG=en_US.UTF-8
-  ##export LC_NUMERIC="en_US.UTF-8"
-  ##export LC_TIME="en_US.UTF-8"
-  ##export LC_COLLATE="en_US.UTF-8"
-  ##export LC_MONETARY="en_US.UTF-8"
-  #export LC_PAPER="en_US.UTF-8"
-  ##export LC_NAME="en_US.UTF-8"
-  ##export LC_ADDRESS="en_US.UTF-8"
-  ##export LC_TELEPHONE="en_US.UTF-8"
-  ##export LC_MEASUREMENT="en_US.UTF-8"
-  ##export LC_IDENTIFICATION="en_US.UTF-8"
-#fi
+#This variable governs the collation rules used for sorting and regular
+#expressions.  Setting the value to C can for example make the ls command sort
+#dotfiles first, followed by uppercase and lowercase filenames: 
+export LC_COLLATE="C"
 
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+#export LC_MESSAGES="en_US.UTF-8"
+#export LC_CTYPE="en_US.UTF-8"
+#export LC_NUMERIC="en_US.UTF-8"
+#export LC_TIME="en_US.UTF-8"
+#export LC_MONETARY="en_US.UTF-8"
+#export LC_PAPER="en_US.UTF-8"
+#export LC_NAME="en_US.UTF-8"
+#export LC_ADDRESS="en_US.UTF-8"
+#export LC_TELEPHONE="en_US.UTF-8"
+#export LC_MEASUREMENT="en_US.UTF-8"
+#export LC_IDENTIFICATION="en_US.UTF-8"
+
+
+#LC_ALL is the only LC_* variable, which cannot be set in locale.conf files: it
+#is meant to be used only for testing or troubleshooting purposes, for example
+#in /etc/profile
+#LC_ALL
