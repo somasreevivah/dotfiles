65c7e67eac73405df1f7f7ad27b5cfcdffc40693
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-05-12 08:56:04 +0000

    p

--- lib/20_locales.sh
+++ lib/20_locales.sh
@@ -14,8 +14,9 @@ export LC_COLLATE="C"
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-#export LC_MESSAGES="en_US.UTF-8"
-#export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="C"
+export LC_CTYPE="C"
+
 #export LC_NUMERIC="en_US.UTF-8"
 #export LC_TIME="en_US.UTF-8"
 #export LC_MONETARY="en_US.UTF-8"
