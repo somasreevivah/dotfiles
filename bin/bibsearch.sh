5ef54d4bf962d55bf272d51a6b8477d224b9f037
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-05-17 13:20:56 +0000

    bibsearch for searching bibtex

---
+++ bin/bibsearch.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+
+
+
+search_string=
+for term in $@
+do
+  search_string="$search_string%20$term"
+done
+
+
+###########################
+#  From bibtexsearch.com  #
+###########################
+
+curl "http://search.bibtexsearch.com/search?auth=web&q=$search_string" | \
+  jq  ".[] | ._source.bibtex" | \
+  sed "s/\\\t/  /g" | \
+  sed "s/\\\n/\n/g" | \
+  sed -e "s/\"@/@/g" -e "s/}\"/}/g" 
