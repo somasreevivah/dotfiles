e898740b8f3f6f39e8e31586cda61af3a287657d
nango <aamsgallo@gmail.com>
2016-06-03 17:06:21 +0200

    Wallpapers

--- bin/set_wallpaper.sh
+++ bin/set_wallpaper.sh
@@ -6,6 +6,6 @@ WALLPAPERS_DIR="${SCRIPT_DIR}/../wallpapers"
 which feh &> /dev/null || exit 1
 
 IMAGE_FILE=$(ls $WALLPAPERS_DIR | sort -R | tail -1)
-feh --bg-center $WALLPAPERS_DIR/$IMAGE_FILE
+feh --bg-fill $WALLPAPERS_DIR/$IMAGE_FILE
 
 
