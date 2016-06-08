e3bc436bcc7352146d76b12220a655fc7a9c1ed3
Alejandro Agusti Martinez-Soria Gallo (119286) <st119286@jorah.physcip.uni-stuttgart.de>
2016-06-08 16:19:51 +0200

    Cask st

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -1,9 +1,6 @@
 # OSX-only stuff. Abort if not OSX.
 dfs_is_osx || return 1
 
-if [[ $(whoami) =~ st* ]]; then
-  export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications --caskroom=$HOME/local/"
-fi
 
 
 # Exit if Homebrew is not installed.
