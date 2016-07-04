d2e34a287964d3265b37043a3d7c84d08382aa24
ale <ale@lenovo.domain>
2016-07-04 23:32:50 +0200

    stuff

--- bin/templi.sh
+++ bin/templi.sh
@@ -3,7 +3,7 @@
 __SCRIPT_VERSION="0.0.1"
 __SCRIPT_NAME=$( basename $0 )
 __DESCRIPTION="Minimal template engine written in bash"
-__OPTIONS=":hvi:o:d"
+__OPTIONS=":hvi:o:dl"
 
 
 function usage_head() { echo "Usage :  $__SCRIPT_NAME [-h|-help] [-v|-version]"; }
@@ -15,16 +15,63 @@ $(usage_head)
     $__DESCRIPTION
 
     Options:
-    -h|help       Display this message
-    -v|version    Display script version
+    -h            Display this message
+    -v            Display script version
     -i            Input template file
-    -o            Output template file
+    -o            Output template file (for outputing
+                  bash scripts with the templates).
+                  If this is set, the result of the template
+                  will not be sent to stdout.
     -d            Verbose mode
+    -l            List templates in path
 
 
     This program is maintained by Alejandro Gallo.
 EOF
 }    # ----------  end of function usage  ----------
+
+
+#  constants {{{1  #
+####################
+
+TEMPLI_PATH="${TEMPLI_PATH}:$HOME/.templi"
+TMP_FILE=/tmp/templi.tmp
+FILE_FOUND=
+
+#  fucntion definition {{{1  #
+##############################
+
+function printv() {
+  if [[ -n ${VERBOSE} ]]; then
+    echo $@
+  fi
+}
+
+
+function format_to_template() {
+  file_to_convert=$1
+  sed '
+  #s/\$/\\$/g #s/\([^ ]\)$ / \\$ /g
+  #s/\([^ ]\)$ / \\$ /g
+  s/\\[^$]/\\\\/g
+  s/\$\([0-9]\)/\\$\1/g
+  ' ${file_to_convert}
+}
+
+
+
+function list_templates() {
+  for path_dir in $(tr ":" "\n" <<<${TEMPLI_PATH}) ;do
+    for file in ${path_dir}/* ; do
+      echo $file
+    done
+  done
+}
+
+
+#  Command line parsing {{{1  #
+###############################
+
 
 while getopts $__OPTIONS opt
 do
@@ -36,9 +83,11 @@ do
 
   i          )  INPUT_TEMPLATE_FILE=$OPTARG ;;
 
-  o          )  OUTPUT_FILE=$OPTARG ;;
+  o          )  OUTPUT_TEMPLATE_FILE=$OPTARG ;;
 
   d          )  VERBOSE=1 ;;
+
+  l          )  list_templates; exit 0;;
 
   * )  echo -e "\n  Option does not exist : $OPTARG\n"
       usage_head; exit 1   ;;
@@ -47,28 +96,12 @@ do
 done
 shift $(($OPTIND-1))
 
-function printv() {
-  if [[ -n ${VERBOSE} ]]; then
-    echo $@
-  fi
-}
-
-function convert_to_template() {
-  file_to_convert=$1
-  sed "
-  #s/\$/\\$/g
-  #s/\([^ ]\)$ / \\$ /g
-  s/\\/\\\\/g
-  " ${file_to_convert}
-}
-
-TEMPLI_PATH="${TEMPLI_PATH}:$HOME/.templi"
-TMP_FILE=/tmp/templi.tmp
-FILE_FOUND=
 
 
+#  main code {{{1  #
+####################
 
-###################
+
 #  look for file  #
 ###################
 printv "Looking for file ${INPUT_TEMPLATE_FILE}"
@@ -118,7 +151,7 @@ $(tr ":" "\n" <<<$@ | while read pair; do echo $pair; done)
 cat <<TEMPLIEOF
 
 
-$(cat ${INPUT_TEMPLATE_FILE})
+$(format_to_template ${INPUT_TEMPLATE_FILE})
 
 
 TEMPLIEOF
@@ -133,5 +166,9 @@ printv "Running the script"
 
 
 chmod +x ${TMP_FILE}
-${TMP_FILE}
+if [[ -z ${OUTPUT_TEMPLATE_FILE} ]]; then
+  ${TMP_FILE}
+else
+  cp ${TMP_FILE} ${OUTPUT_TEMPLATE_FILE}
+fi
 
