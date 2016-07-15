24609a5447d9f6138d3730b6b2f2cdf6d42b5a01
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-07-15 11:34:06 +0200

    added template and updated check function in rosetta

--- link/vim/UltiSnips/rosetta.sh
+++ link/vim/UltiSnips/rosetta.sh
@@ -18,8 +18,10 @@ $(usage_head)
     -v|version    Display script version
     -l            Language for processing options (regex)
 
-    Commands:
-      check       Check the status of the snippets
+    Command:
+     check        Check the status of the snippets for a given language
+                  with -l flag or for all languages
+     template     Print out a template
 
 
     This program is maintained by Alejandro Gallo.
@@ -60,12 +62,12 @@ function success()  { echo -e " \033[1;32mO\033[0m  $@"; }
 function error()    { echo -e " \033[1;31mX\033[0m  $@"; }
 function arrow()    { echo -e " \033[1;34m->>\033[0m  $@"; }
 
-function get_rosetta_snippets() {
+get_rosetta_snippets() {
   declare -x -r SNIPPET_FILENAME=rosetta.snippets
   find . -name $SNIPPET_FILENAME | grep --color=never $PROG_LANGUAGE
 }
 
-function check_triggers() {
+check_triggers() {
   declare -x -r path=$1
   declare -x -r TRIGGERS_DB="./rosetta/triggers.db"
   cat $TRIGGERS_DB | while read trigger_line ; do
@@ -78,7 +80,8 @@ function check_triggers() {
     continue
   fi
 
-  if grep $trigger $path &> /dev/null  ; then
+  snippet_line=$(sed -n "/^snippet\s\+${trigger}/p" ${path} )
+  if [[ -n ${snippet_line} ]]  ; then
     success "\033[0;36m$trigger\033[0m found"
   else
     error "\033[0;36m$trigger\033[0m not found"
@@ -89,10 +92,37 @@ function check_triggers() {
   done
 }
 
+create_template() {
+  declare -x -r TRIGGERS_DB="./rosetta/triggers.db"
+  cat ${TRIGGERS_DB} | while read line; do
+  trigger=$(echo $line | cut -d " " -f 1)
+  description=$(echo $line | cut -d " " -f 2-)
+  if [[ $description = $trigger && -n ${description} ]]; then
+    #main separator
+    echo -e "\n\n### ${trigger} {{{1\n\n"
+    continue
+  fi
+  if [[ -z ${trigger} ]]; then
+    continue
+  fi
+  cat <<EOF
+#snippet ${trigger} "${description}" b
+#endsnippet
+EOF
+
+done
+}
+
 if [[ $COMMAND =~ check ]]; then
-  for file in $(get_rosetta_snippets) ; do
+  for file in $(get_rosetta_snippets ) ; do
     header "\033[1;36mLANGUAGE\033[0m: \033[46m\033[1;93m    $( dirname ${file} | sed "s/\.\///")    \033[0m\033[0m"
     check_triggers $file
   done
+elif [[ ${COMMAND} = template ]]; then
+  create_template
 fi
+
+#vim-run: bash % -l perl check
+#vim-run: bash % template
+
 
