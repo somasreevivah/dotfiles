bb411c021129661c4ab7b085757a87b46c4462de
alejandrogallo <gallo@pccq05.fkf.mpg.de>
2016-08-18 12:42:12 +0200

    local custom bash completion

---
+++ link_config/bash_completion.d/templi.sh
@@ -0,0 +1,34 @@
+
+_templi ()   #  By convention, the function name
+{                 #+ starts with an underscore.
+  local cur
+  # Pointer to current completion word.
+  # By convention, it's named "cur" but this isn't strictly necessary.
+
+  COMPREPLY=()   # Array variable storing the possible completions.
+  cur=${COMP_WORDS[COMP_CWORD]}
+
+  case "$cur" in
+    -*)
+    COMPREPLY=( $( compgen -W '-a -d -f -l -t -h --aoption --debug \
+                               --file --log --test --help --' -- $cur ) );;
+    "-f ")
+      COMPREPLY=($(ls ${HOME}/.templi))
+      ;;
+#   Generate the completion matches and load them into $COMPREPLY array.
+#   xx) May add more cases here.
+#   yy)
+#   zz)
+  esac
+
+  return 0
+}
+
+_list_files ()
+{
+  COMPREPLY=( pepe );
+}
+
+complete -F _templi -o filenames templi.sh
+complete -F _list_files -f templi.sh
+
