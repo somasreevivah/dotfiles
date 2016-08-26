0ef70846fca409e19ddf7fee4ca8ff83b11d86d1
pini <pini@imac.localdomain>
2016-08-26 19:30:21 +0200

    added find flash script

---
+++ bin/find-flash.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+
+__SCRIPT_VERSION="0.0.1"
+__SCRIPT_NAME=$( basename $0 )
+__DESCRIPTION="Get Flash Videos path being streamed in your system."
+__OPTIONS=":hv"
+
+
+function usage_head() { echo "Usage :  $__SCRIPT_NAME [-h|-help] [-v|-version]"; }
+function usage ()
+{
+cat <<EOF
+$(usage_head)
+
+    $__DESCRIPTION
+
+    Options:
+    -h|help       Display this message
+    -v|version    Display script version
+
+
+    This program is maintained by Alejandro Gallo.
+EOF
+}    # ----------  end of function usage  ----------
+
+while getopts $__OPTIONS opt
+do
+  case $opt in
+
+  h|help     )  usage; exit 0   ;;
+
+  v|version  )  echo "$__SCRIPT_NAME -- Version $__SCRIPT_VERSION"; exit 0   ;;
+
+  * )  echo -e "\n  Option does not exist : $OPTARG\n"
+      usage_head; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $(($OPTIND-1))
+
+
+if [[ $(uname) = Darwin ]]; then
+  find /private &>/dev/null | grep "/Flash"
+fi
