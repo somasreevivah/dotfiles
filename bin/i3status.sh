c4620b60f468c4c5181dc62669a53e4ee1d271e5
Alejandro Gallo <aamsgallo@gmail.com>
2016-09-16 17:43:54 +0200

    fix

--- bin/i3status.sh
+++ bin/i3status.sh
@@ -1,5 +1,5 @@
-#status_command i3status --config ~/.i3/i3status.conf
-#status_command i3blocks -c ~/.i3/i3blocks.conf
-#status_command py3status
-#status_command py3status -c ~/.i3/i3status.conf
+#i3blocks -c ~/.i3/i3blocks.conf
+#py3status
+#py3status -c ~/.i3/i3status.conf
 i3blocks -c ~/.i3/i3blocks.conf
+#i3status --config ~/.i3/i3status.conf
