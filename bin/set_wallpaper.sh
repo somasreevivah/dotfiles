e9fd791f1e63b559bc894ef9f07d9af6c599a416
Alejandro Gallo <aamsgallo@gmail.com>
2016-10-09 22:59:35 +0200

    add wallpaperscraft to wallpaper

--- bin/set_wallpaper.sh
+++ bin/set_wallpaper.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+function header()   { echo -e "\n\033[1m$@\033[0m"; }
+function success()  { echo -e " \033[1;32m==>\033[0m  $@"; }
+function error()    { echo -e " \033[1;31mX\033[0m  $@"; }
+function arrow()    { echo -e " \033[1;34m==>\033[0m  $@"; }
+function warning()  { echo -e " \033[0;93m==>\033[0m  $@"; }
+
 WALLPAPERS_DIR=$(readlink -f ~/.dotfiles/wallpapers)
 IMAGE_PATH=${WALLPAPERS_DIR}/temporary.jpg
 
@@ -25,6 +31,33 @@ local_wallpaper() {
   IMAGE_PATH=$WALLPAPERS_DIR/$imageFile
 }
 
+wallpaperscraft() {
+  local url_base="https://wallpaperscraft.com"
+  local resolution="1600x1200"
+  local fmt="jpg"
+  local names
+  local url
+  local modalities=(
+  abstract
+  space
+  abstract
+  textures
+  )
+  local modality=$(echo ${modalities[@]} | tr " " "\n" | sort -R | head -1)
+  echo "Searching for modality ${modality}"
+
+  names=$(curl -s "${url_base}/catalog/${modality}/${resolution}" \
+    | grep img \
+    | sed -n "s/.*src\s*=\s*\"\(.*\.${fmt}\)\".*/\1/p"\
+    | xargs -n1 -I FF echo http:FF \
+    | sed "s/[0-9]\+x[0-9]\+\.${fmt}/${resolution}.${fmt}/"
+  )
+  #base_url/image/black_background_red_color_paint_explosion_burst_9844_1600x1200.jpg
+  url=$(echo ${names} | tr " " "\n" | sort -R | head -1 )
+  echo "Downloading ${url} ... "
+  wget ${url} -O ${IMAGE_PATH} || wall_notify "Failure"
+}
+
 hubble() {
   #fetch images from
   #http://hubblesite.org/gallery/wallpaper
@@ -45,15 +78,27 @@ hubble() {
   echo "$number"
   echo "${tmp}"
   url="http://imgsrc.hubblesite.org/hu/db/images/hs-${year}-${number}-a-${resolution}_wallpaper.jpg"
+  echo "Downloading ${url} ... "
   wget ${url} -O ${IMAGE_PATH} || wall_notify "Failure"
 }
+
+
+
 
 PARSERS=(
 local_wallpaper
 hubble
+wallpaperscraft
 )
 
-parse=$(echo ${PARSERS[@]} | tr " " "\n" | sort -R | tail -1)
+
+if [[ -n $1 ]]; then
+  parse=$1
+else
+  parse=$(echo ${PARSERS[@]} | tr " " "\n" | sort -R | tail -1)
+fi
+
+arrow "Using ${parse}"
 
 type feh 2>&1 > /dev/null || exit 1
 
@@ -64,5 +109,5 @@ ${parse}
 
 feh --bg-max $IMAGE_PATH || wall_notify "Failure setting wallpaper"
 
-#vim-run: bash %
+#vim-run: bash % wallpaperscraft
 
