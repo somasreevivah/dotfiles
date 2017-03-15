bdf904238fda8df119f29280b2d2209a1ab80348
Alejandro Gallo <aamsgallo@gmail.com>
2017-03-16 00:56:12 +0500

    Fix

--- bin/terminal.sh
+++ bin/terminal.sh
@@ -4,7 +4,7 @@
 if which urxvt; then
   # Because it works well with ranger for images
   urxvt $*
-if which lilyterm; then
+elif which lilyterm; then
   lilyterm $*
 elif which konsole; then
   konsole $*
