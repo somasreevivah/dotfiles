b6c6a3df102b07217a9603c1eb953946f4dbf5b3
alejandrogallo <aamsgallo@gmail.com>
2017-04-10 11:15:42 +0200

    improve bcmp

--- bin/bcmp.py
+++ bin/bcmp.py
@@ -2,60 +2,85 @@
 # -*- coding: utf-8 -*-
 
 
-import urllib.request
+# import urllib.request
 import re
 import json
 import os
 import sys
+import eyed3
+from six.moves import urllib
 
-# url = "https://nango.bandcamp.com/releases"
-url = sys.argv[1]
+def download(url):
+    """TODO: Docstring for download.
 
-print("Input url %s" % url)
-print("Parsing url...")
+    :url: TODO
+    :returns: TODO
 
-m = re.match(r"(.*).bandcamp.com.*", url)
-if m:
-    artist = m.group(1)\
-              .replace("https://","")\
-              .replace("//","")
-    album = None
-    folder = artist
+    """
+    print("Input url %s" % url)
+    print("Parsing url...")
 
-m = re.match(r"(.*).bandcamp.com/album/(.*)", url)
-if m:
-    artist = m.group(1)\
-              .replace("https://","")\
-              .replace("//","")
-    album = m.group(2)
-    folder = os.path.join(artist, album)
+    m = re.match(r"(.*).bandcamp.com.*", url)
+    if m:
+        artist = m.group(1)\
+                  .replace("https://","")\
+                  .replace("//","")
+        album = None
+        folder = artist
 
-print("Artist: %s" % artist)
-if album: print("Album: %s" % album)
+    m = re.match(r"(.*).bandcamp.com/album/(.*)", url)
+    if m:
+        artist = m.group(1)\
+                  .replace("https://","")\
+                  .replace("//","")
+        album = m.group(2)
+        folder = os.path.join(artist, album)
 
-print("Downloading website...")
-data = urllib.request.urlopen(url).read().decode('utf-8')
+    print("Artist: %s" % artist)
+    if album: print("Album: %s" % album)
 
-print("Parsing data...")
-for m in re.findall(r".*trackinfo:\s*(\[.*\]).*", data):
-    tracks = json.loads(m)
+    print("Downloading website...")
+    data = urllib.request.urlopen(url).read().decode('utf-8')
 
-if not os.path.exists(folder):
-    print("Creating dir %s" % folder)
-    os.makedirs(folder, mode=0o777)
+    print("Parsing data...")
+    for m in re.findall(r".*trackinfo:\s*(\[.*\]).*", data):
+        tracks = json.loads(m)
 
-for track in tracks:
-    title = track.get("title")
-    track_num = track.get("track_num")
-    url = "https:"+track.get("file").get("mp3-128")
-    print("%s. %s (%s)" % (
-        track_num,
-        title,
-        url
-        ))
-    print("\tDownloading....")
-    fd = open(folder+"/%s_%s.mp3"%(track_num, title.replace(" ", "-")), "wb+")
-    fd.write(urllib.request.urlopen(url).read())
-    fd.close()
+    if not os.path.exists(folder):
+        print("Creating dir %s" % folder)
+        os.makedirs(folder, mode=0o777)
 
-#vim-run: python3 % https://kennelpanic.bandcamp.com/album/the-johnny-silver-ep
+    for track in tracks:
+        title = track.get("title")
+        track_num = track.get("track_num")
+        song_path = folder+"/%s_%s.mp3"%(track_num, title.replace(" ", "-"))
+        url = "https:"+track.get("file").get("mp3-128")
+        print("%s. %s (%s)" % (
+            track_num,
+            title,
+            url
+            ))
+        if not os.path.exists(song_path):
+            print("\tDownloading....")
+            fd = open(song_path, "wb+")
+            fd.write(urllib.request.urlopen(url).read())
+            fd.close()
+        print("\t* Setting mp3 metadata...")
+        audio_file = eyed3.load(song_path)
+        audio_file.initTag()
+        print("\t\t - artist")
+        audio_file.tag.artist = unicode(artist)
+        print("\t\t - album")
+        audio_file.tag.album = unicode(album)
+        print("\t\t - title")
+        audio_file.tag.title = unicode(title)
+        print("\t\t - track_num")
+        audio_file.tag.track_num = track_num
+        audio_file.tag.save()
+
+if __name__ == "__main__":
+    urls = sys.argv[1:]
+    for url in urls:
+        download(url)
+
+#vim-run: python % https://kennelpanic.bandcamp.com/album/the-johnny-silver-ep
