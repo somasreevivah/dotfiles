7e6f1d7781ea777a4f98ff7e4a8db4d4ca62986e
alejandrogallo <aamsgallo@gmail.com>
2017-04-10 10:50:08 +0200

    bandcamp better

--- bin/bcmp.py
+++ bin/bcmp.py
@@ -11,18 +11,38 @@ import sys
 # url = "https://nango.bandcamp.com/releases"
 url = sys.argv[1]
 
-data = urllib.request.urlopen(url).read().decode('utf-8')
-artist = re.match(r"(.*).bandcamp.com.*", url).group(1)\
-        .replace("https://","")\
-        .replace("//","")
-print("Artist: %s" % artist)
+print("Input url %s" % url)
+print("Parsing url...")
 
+m = re.match(r"(.*).bandcamp.com.*", url)
+if m:
+    artist = m.group(1)\
+              .replace("https://","")\
+              .replace("//","")
+    album = None
+    folder = artist
+
+m = re.match(r"(.*).bandcamp.com/album/(.*)", url)
+if m:
+    artist = m.group(1)\
+              .replace("https://","")\
+              .replace("//","")
+    album = m.group(2)
+    folder = os.path.join(artist, album)
+
+print("Artist: %s" % artist)
+if album: print("Album: %s" % album)
+
+print("Downloading website...")
+data = urllib.request.urlopen(url).read().decode('utf-8')
+
+print("Parsing data...")
 for m in re.findall(r".*trackinfo:\s*(\[.*\]).*", data):
     tracks = json.loads(m)
 
-if not os.path.exists(artist):
-    print("Creating dir %s" % artist)
-    os.mkdir(artist, mode=0o777)
+if not os.path.exists(folder):
+    print("Creating dir %s" % folder)
+    os.makedirs(folder, mode=0o777)
 
 for track in tracks:
     title = track.get("title")
@@ -34,7 +54,8 @@ for track in tracks:
         url
         ))
     print("\tDownloading....")
-    fd = open(artist+"/%s_%s.mp3"%(track_num, title.replace(" ", "-")), "wb+")
+    fd = open(folder+"/%s_%s.mp3"%(track_num, title.replace(" ", "-")), "wb+")
     fd.write(urllib.request.urlopen(url).read())
     fd.close()
 
+#vim-run: python3 % https://kennelpanic.bandcamp.com/album/the-johnny-silver-ep
