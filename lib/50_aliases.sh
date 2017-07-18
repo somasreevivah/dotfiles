de942d378785c0585a7e9eedb134ec858fbd7c19
Alejandro Gallo <aamsgallo@gmail.com>
2017-07-18 15:52:47 +0200

    zat

--- lib/50_aliases.sh
+++ lib/50_aliases.sh
@@ -6,3 +6,12 @@ alias chromium="chromium --app='https://google.com'"
 alias grep="grep --color=auto"
 alias bookis="papis -l books"
 alias conis="papis -l contacts"
+
+zat() {
+  for i in $@; do
+    [[ -f $i ]] && file=$i
+  done
+  [[ -n ${file} ]] &&
+  zathura --data-dir=$(dirname $file)/zathura_cache/ $@ ||
+  zathura $@
+}
