9667bb56ec74913fe00dd1b5d5d38f0eadf96ddb
Alejandro Gallo <aamsgallo@gmail.com>
2017-08-11 19:02:13 +0200

    aljazeera

--- bin/aljazeera.sh
+++ bin/aljazeera.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 url=https://www.youtube.com/watch?v=fN2sEo7hq-E
+doc_url=https://www.youtube.com/watch?v=ZJIDMi_8O5o
+
+if [[ $1 == doc ]]; then
+  echo "Aljazeera documentaries"
+  url=${doc_url}
+else
+  echo "Aljazeera news"
+fi
 
 mpv ${url} ||
 mpsyt playurl ${url} ||
