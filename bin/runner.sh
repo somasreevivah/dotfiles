919dfdf1cfd26cabbe0a1b02a07ff1142a199bee
Alejandro Gallo <aamsgallo@gmail.com>
2017-08-14 09:10:54 +0200

    runner

--- bin/runner.sh
+++ bin/runner.sh
@@ -4,8 +4,11 @@
 
 # In general do this
 if which rofi >&1 2> /dev/null; then
-  #rofi -show run
-  rofi -show combi
+  if [[ $(hostname) == cqc02 ]]; then
+    rofi -show run
+  else
+    rofi -show combi
+  fi
   exit $?
 elif which dmenu_run >&1 2> /dev/null; then
   dmenu_run -i -l 10
