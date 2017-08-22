bd50236bb5a7d992ab880a985e47c69215441eaa
Alejandro Gallo <aamsgallo@gmail.com>
2017-08-22 15:17:46 +0200

    trainer

--- bin/bin-trainer.rb
+++ bin/bin-trainer.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
+require "colorize"
+
 $max_word_length = 4
 $symbols = ["0", "1"]
+$correct = 0
+$wrong = 0
 
 def get_random_non_decimal_string
   number = ""
@@ -16,18 +20,27 @@ end
 def main
   while true
     number = get_random_non_decimal_string
+    puts "\n=================="
     puts "What is it? #{number}"
     answer = gets
     if answer.to_i == number.to_i($symbols.length)
-      puts "Correct !"
+      puts "Correct !".colorize(:green)
+      $correct += 1
     else
-      puts "Shame on you! it is #{number.to_i(2)} !"
+      puts "Wrong! It is ".colorize(:red) + "#{number.to_i(2)}".colorize(:green)
+      $wrong += 1
     end
   end
 end
 
+def print_stats
+  puts "\n\nYou got " + $correct.to_s.colorize(:green) + " correct and " + \
+    $wrong.to_s.colorize(:red) + " wrong"
+end
+
 if __FILE__ == $PROGRAM_NAME
   Signal.trap('INT') do
+    print_stats
     puts "\nI'm getting you outta here"
     exit 0
   end
