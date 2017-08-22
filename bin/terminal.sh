253edfe5472d5d75b362af3bf34ee421443a1696
Alejandro Gallo <aamsgallo@gmail.com>
2017-08-22 15:17:49 +0200

    stuff

--- bin/terminal.sh
+++ bin/terminal.sh
@@ -3,7 +3,7 @@
 {
 if which urxvt; then
   # Because it works well with ranger for images
-  urxvt $*
+  urxvt $([[ -f ~/.flags/urxvt ]] && cat ~/.flags/urxvt) $*
 elif which lilyterm; then
   lilyterm $*
 elif which konsole; then
