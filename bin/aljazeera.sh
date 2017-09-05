e361ace256bfbfd858829e971b0b9d9242bbe552
Alejandro Gallo <aamsgallo@gmail.com>
2017-09-05 18:19:25 +0200

    Submodule upload

--- bin/aljazeera.sh
+++ bin/aljazeera.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
-url=https://www.youtube.com/watch?v=fN2sEo7hq-E # old url
+#url=https://www.youtube.com/watch?v=fN2sEo7hq-E # old url
 url=https://www.youtube.com/watch?v=0zc9dACzJYY
-doc_url=https://www.youtube.com/watch?v=ZJIDMi_8O5o
+#doc_url=https://www.youtube.com/watch?v=ZJIDMi_8O5o #old
+doc_url=https://www.youtube.com/watch?v=Ntgv6zrhUZQ
 
 if [[ $1 == doc ]]; then
   echo "Aljazeera documentaries"
