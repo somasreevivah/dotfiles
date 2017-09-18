8821a243ae7bb7fc05ab978ca89b3dfb842e0c53
Alejandro Gallo <aamsgallo@gmail.com>
2017-09-18 19:11:20 +0200

    Aliases

--- lib/50_aliases.sh
+++ lib/50_aliases.sh
@@ -4,9 +4,11 @@ alias octave="octave --silent"
 alias xa="xargs -n1"
 alias chromium="chromium --app='https://google.com'"
 alias grep="grep --color=auto"
+
 alias bookis="papis -l books"
 alias conis="papis -l contacts"
 alias vapis="papis --pick-lib --picktool vim gui --vim"
+alias theis="papis -l /home/tsatsoulis/Documents/papers"
 
 rgrep() {
   grep -r "$@"
