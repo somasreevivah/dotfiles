947b43db8e60adf74cd1f0befec3e0c89979af70
Alejandro Gallo <aamsgallo@gmail.com>
2017-10-04 00:01:30 +0200

    Implement more operations

--- bin/indi.py
+++ bin/indi.py
@@ -1,8 +1,39 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+>>> a = VerticalReflexion() % HorizontalReflexion()
+>>> a.__mul__('abcd')
+'dcba'
+>>> a * 'vovo'
+'ovov'
+"""
 import sets
+import itertools
 
-class VerticalReflexion(object):
+
+class GroupOperation(object):
+    """
+    >>> GroupOperation() * 'asdf'
+    """
+    mul_method = lambda self, string: None
+    def __mod__(self, other):
+        result = GroupOperation()
+        result.mul_method = lambda string: self * ( other * string )
+        return result
+
+    def __mul__(self, string):
+        return self.mul_method(string)
+
+
+class Identity(GroupOperation):
+    """
+    >>> Identity() * 'asdf'
+    'asdf'
+    """
+    mul_method = lambda self, string: string
+
+
+class VerticalReflexion(GroupOperation):
     def __mul__(self, string):
         """
         >>> VerticalReflexion()*'vovo'
@@ -12,11 +43,27 @@ class VerticalReflexion(object):
         """
         middle = len(string)/2
         middle_middle = middle/2
-        return string[0:middle][middle_middle:] + string[0:middle][0:middle_middle] \
-                + string[middle:][middle_middle:] + string[middle:][0:middle_middle]
+        return string[0:middle][middle_middle:] \
+            + string[0:middle][0:middle_middle] \
+            + string[middle:][middle_middle:] \
+            + string[middle:][0:middle_middle]
 
 
-class HorizontalReflexion(object):
+class VerticalReflexionTop(GroupOperation):
+    def __mul__(self, string):
+        """
+        >>> VerticalReflexionTop() * 'abcd'
+        'bacd'
+        >>> HorizontalReflexion() % VerticalReflexionTop() % HorizontalReflexion() * 'abcd'
+        'abdc'
+        """
+        middle = len(string)/2
+        return string[0:middle][1] \
+            + string[0:middle][0] \
+            + string[middle:]
+
+
+class HorizontalReflexion(GroupOperation):
     def __mul__(self, string):
         """
         >>> HorizontalReflexion()*'abcd'
@@ -28,6 +75,21 @@ class HorizontalReflexion(object):
         return string[middle:] + string[0:middle]
 
 
+class HorizontalReflexionLeft(GroupOperation):
+    def __mul__(self, string):
+        """
+        >>> HorizontalReflexionLeft()*'abcd'
+        'cbad'
+        >>> HorizontalReflexionLeft()*'abcdef'
+        'dbcaef'
+        >>> HorizontalReflexionLeft()*'ab'
+        'ba'
+        """
+        middle = len(string)/2
+        return string[middle:][0] + string[0:middle][1:] \
+            + string[0:middle][0] + string[middle:][1:]
+
+
 def orbit(string):
     result = [string]
     result.append(HorizontalReflexion() * string)
@@ -35,6 +97,19 @@ def orbit(string):
     result.append(HorizontalReflexion() * (VerticalReflexion() * string))
     result.append(VerticalReflexion() * (HorizontalReflexion() * string))
     return sets.Set(result)
+
+
+e = Identity()
+h = HorizontalReflexion()
+v = VerticalReflexion()
+
+G_complex = [e, h, v, h % v]
+
+print(G_complex)
+
+SPACE = ["".join(d) for d in itertools.product('vo', repeat=4)]
+
+print(SPACE)
 
 BASE = sets.Set([
     "vvvv",
@@ -64,5 +139,6 @@ for c in CANDIDATES:
         print('\t\t %s No representable' % c)
 
 
-#vim-run: python -m doctest %
-#vim-run: python %
+# vim-run: python -m doctest %
+# vim-run: flake8 %
+# vim-run: python %
