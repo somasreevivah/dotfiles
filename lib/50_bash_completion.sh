05937a9dd5df7d9fe51d39745bef7bf0360c0354
Alejandro Gallo <aamsgallo@gmail.com>
2018-01-29 09:59:24 +0100

    stuff

--- lib/50_bash_completion.sh
+++ lib/50_bash_completion.sh
@@ -1,7 +1,8 @@
 dfs_log sourcing 50_bash_completion.sh
 # I will ignore it until I find out what is slowing
 # down the sourcing of the bahsrc
-return
+
+#ETC IS slowing everything down
 
 COMPLETION_PATHS=(
 /etc/
@@ -12,9 +13,11 @@ $HOME/local/etc/
 
 for path in ${COMPLETION_PATHS[@]} ; do
   if [ -f ${path}/bash_completion ]; then
+    dfs_log "  sourcing ${path}/bash_completion"
     . ${path}/bash_completion
   elif [ -d ${path}/bash_completion.d ]; then
     for script in  ${path}/bash_completion.d/*; do
+      dfs_log "  sourcing script ${script}"
       . ${script}
     done
   fi
