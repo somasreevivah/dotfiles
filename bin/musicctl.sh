e61f90a01078857c305f121932236528d9622149
Alejandro Gallo <aamsgallo@gmail.com>
2018-03-01 22:36:56 +0100

    ias

--- bin/musicctl.sh
+++ bin/musicctl.sh
@@ -5,20 +5,24 @@ cmd=$1
 mpsytpid=$(xdotool search --name mpsyt)
 echo ${mpsytpid}
 
-
-notify-send \
-  -i notification-audio-${cmd} \
-  -t 500 ${cmd} "Pid ${mpsytpid} $USER"
+notify(){
+  notify-send \
+    -i notification-audio-${cmd} \
+    -t 500 ${cmd} "Pid ${mpsytpid} $USER"
+}
 
 if [ ${cmd} == pause ] || [ ${cmd} == play ] || [ ${cmd} == stop ]; then
+  notify
   xdotool key --window ${mpsytpid} p
 elif [ ${cmd} == rewind ]; then
-  xdotool type --window ${mpsytpid} left
+  xdotool type --window ${mpsytpid} down
 elif [ ${cmd} == forward ]; then
-  xdotool type --window ${mpsytpid} right
+  xdotool type --window ${mpsytpid} up
 elif [ ${cmd} == next ]; then
+  notify
   xdotool type --window ${mpsytpid} '>'
 elif [ ${cmd} == previous ]; then
+  notify
   xdotool type --window ${mpsytpid} '<'
 fi
 
